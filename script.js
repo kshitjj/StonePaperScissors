28a796b73152f5a942e5fb65e06b6c863630644b
kshtijj <kshitijrajgude@gmail.com>
2022-07-23 01:25:21 +0530

    added comments to guide me the backend

---
+++ script.js
@@ -0,0 +1,15 @@
+// Stone Paper Scissors Game
+// input comes in asking whether you want to play the game or not
+//  if yes, then we start the game(). game is best of three, which means 5 rounds.
+//      We take input from user in form of rock, paper, scissors. 
+//      Input is then compared with a computer generated 
+//          computer generated input is obtained with the help of Math.random
+//      The input from the user and the computer are compared and you get output in form of console.log("you lost or You Win").
+//          if W, then +1 Human,
+//          if L, then +1 Computer    
+//      First one to get to 3 W's Wins the game.
+//      After winning the game, You are asked if you want to help again. 
+//          If yes, then game().
+//          If no, say your good byes
+//  else, then say our good byes
+// 
